721e11ce1162b29ce4b8961db312c131db2b7e0d
Mathieu <mathieuparisot@hotmail.fr>
2018-11-12 15:28:35 +0100

    CPT

--- wp-content/themes/BeGreen/inc/cpt_petition.php
+++ wp-content/themes/BeGreen/inc/cpt_petition.php
@@ -1,44 +1,47 @@
 <?php
 add_action( 'init', 'ajout_custom_type_petition' );
-
 function ajout_custom_type_petition()
 {
-
 $post_type = "petition";
 $labels = array(
-        'name'               => 'petitions',
-        'singular_name'      => 'petition',
-        'all_items'          => 'Tous les petitions',
-        'add_new'            => 'Ajouter un petition',
-        'add_new_item'       => 'Ajouter un petition',
-        'edit_item'          => "Editer le petition",
-        'new_item'           => 'Nouveau petition',
-        'view_item'          => "Voir le petition",
-        'search_items'       => 'Chercher un petition',
+        'name'               => 'Pétitions',
+        'singular_name'      => 'Pétition',
+        'all_items'          => 'Toutes les pétitions',
+        'add_new'            => 'Ajouter une pétition',
+        'add_new_item'       => 'Ajouter une pétition',
+        'edit_item'          => "Editer la pétition",
+        'new_item'           => 'Nouvelle pétition',
+        'view_item'          => "Voir la pétition",
+        'search_items'       => 'Chercher une pétition',
         'not_found'          => 'Pas de résultat',
         'not_found_in_trash' => 'Pas de résultat',
-        'parent_item_colon'  => 'petition Parent',
+        'parent_item_colon'  => 'Petition Parent',
         'menu_name'          => 'Pétitions',
     );
-
     $args = array(
         'labels'              => $labels,
-        'hierarchical'        => true,
+        'hierarchical'        => false,
         'supports'            => array( 'title','thumbnail','editor', 'revisions'),
         'public'              => true,
         'show_ui'             => true,
         'show_in_menu'        => true,
         'menu_position'       => 5,
         'menu_icon'           => 'dashicons-welcome-write-blog',
-        'show_in_nav_menus'   => false,
+        'show_in_nav_menus'   => true,
         'publicly_queryable'  => true,
         'exclude_from_search' => false,
         'has_archive'         => false,
         'query_var'           => true,
-        'can_export'          => false,
+        'can_export'          => true,
         'rewrite'             => array( 'slug' => $post_type )
     );
-
   register_post_type($post_type, $args );
-
+  $taxonomy = "theme";
+  $object_type = array("petition");
+  $args = array(
+          'label' => __( 'Thématique' ),
+          'rewrite' => array( 'slug' => 'theme' ),
+          'hierarchical' => true,
+  );
+  register_taxonomy( $taxonomy, $object_type, $args );
 }
