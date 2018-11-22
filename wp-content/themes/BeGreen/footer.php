b12333029fd35bbfc353b3989346df6ad1a020ec
Mathieu <mathieuparisot@hotmail.fr>
2018-11-22 09:18:53 +0100

    Push push push

---
+++ wp-content/themes/BeGreen/footer.php
@@ -0,0 +1,29 @@
+<footer>
+        <div class="container">
+            <div class="social">
+                <a href="#">facebook </a>
+                <a href="#">twitter </a>
+                <a href="#">instagram </a>
+                <a href="#">google plus </a>
+                <a href="#">behance </a>
+                <a href="#">dribbble </a>
+            </div>
+            <p>Copyright © 2018 Hetic, All rights Reserved. <br />
+                Created by tavonline</p>
+        </div>
+    </footer>
+
+    <!-- Javascripts -->
+    <script src="wp-content/themes/begreen/js/jquery-2.1.4.min.js"></script><!-- jQuery library -->
+    <script src="wp-content/themes/begreen/js/smoothscroll.js"></script>
+    <script src="wp-content/themes/begreen/js/bootstrap.min.js"></script>
+    <script src="wp-content/themes/begreen/js/wow.min.js"></script>
+    <script src="wp-content/themes/begreen/js/isotope.pkgd.min.js"></script>
+    <script src="wp-content/themes/begreen/js/typed.js"></script>
+    <script src="wp-content/themes/begreen/js/jquery.magnific-popup.min.js"></script>
+    <script src="wp-content/themes/begreen/js/owl.carousel.min.js"></script>
+    <script src="wp-content/themes/begreen/js/main.js"></script>
+
+        <?php wp_footer();  ?>
+    </body>
+</html>
---
+++ wp-content/themes/BeGreen/header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <title>BeGreen - Agissons pour l'environnement</title>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Stylesheets -->
+    <link rel="stylesheet" href="wp-content/themes/begreen/css/bootstrap.css" />
+    <link rel="stylesheet" href="wp-content/themes/begreen/css/reset.css" />
+    <link rel="stylesheet" href="wp-content/themes/begreen/css/style.css" />
+    <link rel="stylesheet" href="wp-content/themes/begreen/css/animate.css" />
+    <link rel="stylesheet" href="wp-content/themes/begreen/css/owl.carousel.css" />
+    <link rel="stylesheet" href="wp-content/themes/begreen/css/magnific-popup.css" />
+    <!-- Google Web fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" rel="stylesheet">
+    <!-- Font icons -->
+    <link rel="stylesheet" href="wp-content/themes/begreen/icon-fonts/font-awesome-4.5.0/css/font-awesome.min.css" />
+    <link rel="stylesheet" href="wp-content/themes/begreen/icon-fonts/essential-regular-fonts/essential-icons.css" />
+    <!--[if lt IE 9]>
+  <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+<![endif]-->
+<?php wp_head(); ?>
+</head>
+
